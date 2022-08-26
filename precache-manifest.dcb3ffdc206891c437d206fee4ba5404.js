5389d72b2730be9666a91afdd8652959c0ffdd57
Mari <maritzarg2020@gmail.com>
2022-08-25 22:04:01 -0500

    Updates

---
+++ precache-manifest.dcb3ffdc206891c437d206fee4ba5404.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "392fe1d1950b1549e9b6",
+    "url": "/mini_challenge/static/css/main.75cfb1fb.chunk.css"
+  },
+  {
+    "revision": "392fe1d1950b1549e9b6",
+    "url": "/mini_challenge/static/js/main.392fe1d1.chunk.js"
+  },
+  {
+    "revision": "79b6f4b813496f1db206",
+    "url": "/mini_challenge/static/js/1.79b6f4b8.chunk.js"
+  },
+  {
+    "revision": "50fce3dabbfc95ee8f71",
+    "url": "/mini_challenge/static/js/runtime~main.50fce3da.js"
+  },
+  {
+    "revision": "c0c9bb4083bace436f9819fe7f4fcf0f",
+    "url": "/mini_challenge/index.html"
+  }
+];
